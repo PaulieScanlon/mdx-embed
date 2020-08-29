c2bc9565bab7876882f0e848d471ff310acc7157
pauliescanlon <pauliescanlon@gmail.com>
2020-08-29 16:44:29 +0100

    add theme-ui

--- docs/.storybook/manager.js
+++ docs/.storybook/manager.js
@@ -1,42 +1,47 @@
 import { addons } from '@storybook/addons';
 import { create } from '@storybook/theming/create';
 
-const theme = create({
-  base: 'light',
+import theme from '../src/theme';
 
-  colorPrimary: '#fcb42d',
-  colorSecondary: 'black',
-
-  // UI
-  appBg: 'white',
-  appContentBg: 'white',
-  appBorderColor: 'white',
-  appBorderRadius: 0,
-
-  // Typography
-  fontBase: '"system-ui", sans-serif',
-  fontCode: '"Roboto Mono", Menlo, monospace',
-
-  // Text colors
-  textColor: 'black',
-  textInverseColor: 'rgba(255,255,255,0.9)',
-
-  // Toolbar default and active colors
-  barTextColor: '#ccc',
-  barSelectedColor: 'black',
-  barBg: 'white',
-
-  // Form colors
-  inputBg: 'white',
-  inputBorder: '#ccc',
-  inputTextColor: 'black',
-  inputBorderRadius: 4,
-
-  brandTitle: 'MDX Embed',
-  brandUrl: 'https://www.mdx-embed.com/',
-  brandImage: '/mdx-embed-logo.svg'
-});
+const {
+  colors: { primary, text, background, grey },
+  fonts: { body, monospace },
+} = theme;
 
 addons.setConfig({
-  theme
+  theme: create({
+    base: 'light',
+
+    colorPrimary: primary,
+    colorSecondary: text,
+
+    // UI
+    appBg: background,
+    appContentBg: background,
+    appBorderColor: background,
+    appBorderRadius: 0,
+
+    // Typography
+    fontBase: body,
+    fontCode: monospace,
+
+    // Text colors
+    textColor: text,
+    textInverseColor: 'rgba(255,255,255,0.9)',
+
+    // Toolbar default and active colors
+    barTextColor: grey,
+    barSelectedColor: text,
+    barBg: background,
+
+    // Form colors
+    inputBg: background,
+    inputBorder: grey,
+    inputTextColor: text,
+    inputBorderRadius: 4,
+
+    brandTitle: 'MDX Embed',
+    brandUrl: 'https://www.mdx-embed.com/',
+    brandImage: '/mdx-embed-logo.svg',
+  }),
 });
--- docs/.storybook/preview.js
+++ docs/.storybook/preview.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import { addParameters } from '@storybook/react';
 import { DocsContainer } from '@storybook/addon-docs/blocks';
+import { MDXProvider } from '@mdx-js/react';
+import { ThemeProvider } from 'emotion-theming';
+import * as ThemeUIComponents from 'theme-ui';
+import { Heading } from 'theme-ui';
+
+import theme from '../src/theme';
 
 import { MDXEmbedProvider } from 'mdx-embed';
 
@@ -8,8 +14,25 @@ addParameters({
   docs: {
     container: ({ children, context }) => (
       <DocsContainer context={context}>
-        <MDXEmbedProvider>{children}</MDXEmbedProvider>
+        <ThemeProvider theme={theme}>
+          <MDXProvider components={ThemeUIComponents}>
+            <MDXEmbedProvider>{children}</MDXEmbedProvider>
+          </MDXProvider>
+        </ThemeProvider>
       </DocsContainer>
-    )
-  }
+    ),
+  },
 });
+
+export const parameters = {
+  docs: {
+    components: {
+      h1: ({ children }) => <Heading as="h1">{children}</Heading>,
+      h2: Heading,
+      h3: ({ children }) => <Heading as="h3">{children}</Heading>,
+      h4: ({ children }) => <Heading as="h4">{children}</Heading>,
+      h5: ({ children }) => <Heading as="h5">{children}</Heading>,
+      h6: ({ children }) => <Heading as="h6">{children}</Heading>,
+    },
+  },
+};
