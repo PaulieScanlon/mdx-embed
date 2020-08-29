ddf62d769ef432774ad61d70a21f88c709427ff9
pauliescanlon <pauliescanlon@gmail.com>
2020-08-29 18:16:50 +0100

    add twitter components

--- packages/mdx-embed/src/index.ts
+++ packages/mdx-embed/src/index.ts
@@ -1,4 +1,14 @@
 // Components
 export { CodePen } from './components/codepen';
+
+export {
+  Tweet,
+  TwitterFollowButton,
+  TwitterHashtagButton,
+  TwitterList,
+  TwitterMentionButton,
+  TwitterTimeline,
+} from './components/twitter';
+
 // Provider
 export { MDXEmbedProvider } from './components/mdx-embed-provider';
