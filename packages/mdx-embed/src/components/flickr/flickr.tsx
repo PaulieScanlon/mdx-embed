21999320ceee83fca66238035842052c4e705c14
pauliescanlon <pauliescanlon@gmail.com>
2020-08-31 14:47:12 +0100

    add Flickr load script to utils

--- packages/mdx-embed/src/components/flickr/flickr.tsx
+++ packages/mdx-embed/src/components/flickr/flickr.tsx
@@ -1,12 +1,13 @@
 import React, { FunctionComponent } from 'react';
 import { GeneralObserver } from '../general-observer';
+import { handleFlickrLoad } from './utils';
 export interface IFlickrProps {
   /** Flickr image link*/
   flickrLink: string;
 }
 
 export const Flickr: FunctionComponent<IFlickrProps> = ({ flickrLink }: IFlickrProps) => (
-  <GeneralObserver>
+  <GeneralObserver onEnter={() => handleFlickrLoad()}>
     <span className="flickr-embed-mdx" data-flickr-embed="true" data-header="true" data-footer="true">
       <img src={`https://live.staticflickr.com/${flickrLink}`} width="100%" height="auto" alt={flickrLink} />
     </span>
