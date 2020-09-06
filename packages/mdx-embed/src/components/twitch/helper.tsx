6b5a2992e5d697e773077a27fdb9e9bcfed651b0
Gabi <gabrielaradu.developer@gmail.com>
2020-09-06 15:01:37 +0300

    The missing helper method.

---
+++ packages/mdx-embed/src/components/twitch/helper.tsx
@@ -0,0 +1,18 @@
+import {isEmptyString} from "../../utils";
+
+export default class Helper {
+
+    static constructTwitchURL(twitchId?: string, channel?: string, collection?: string, baseUrl?: string) {
+        const srcBlocks = [baseUrl];
+
+        if (twitchId !== undefined) {
+            srcBlocks.push(`&video=v${twitchId}`);
+        } else if (!isEmptyString(channel)) {
+            srcBlocks.push(`&channel=${channel}`);
+        } else if (!isEmptyString(collection)) {
+            srcBlocks.push(`&collection=${collection}`);
+        }
+
+        return srcBlocks.join('');
+    }
+}
