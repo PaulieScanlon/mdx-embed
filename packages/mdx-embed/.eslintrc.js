00a8492a2b9bb936a6f1ce0474e69b34a20100de
pauliescanlon <pauliescanlon@gmail.com>
2020-09-15 20:48:07 +0100

    update lint rules

--- packages/mdx-embed/.eslintrc.js
+++ packages/mdx-embed/.eslintrc.js
@@ -2,13 +2,11 @@ module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
-
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
-    'prettier',
     'prettier/@typescript-eslint',
   ],
   settings: {
@@ -17,6 +15,7 @@ module.exports = {
     },
   },
   rules: {
+    'no-loop-func': 0,
     'react/prop-types': 0,
     '@typescript-eslint/no-explicit-any': 0,
     '@typescript-eslint/ban-ts-comment': 0,
@@ -24,5 +23,16 @@ module.exports = {
     '@typescript-eslint/explicit-module-boundary-types': 0,
     '@typescript-eslint/no-empty-interface': 0,
     '@typescript-eslint/no-non-null-assertion': 0,
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        selector: 'interface',
+        format: ['PascalCase'],
+        custom: {
+          regex: '^I[A-Z]',
+          match: true,
+        },
+      },
+    ],
   },
 };
