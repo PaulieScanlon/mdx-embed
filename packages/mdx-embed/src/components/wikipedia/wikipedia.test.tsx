0055cd0f20827bc2f9a9fc95b41b5465164c5f7f
Rémi Roycourt <remiroycourt@gmail.com>
2020-09-25 01:38:40 +0200

    test(wikipedia): add unit test

---
+++ packages/mdx-embed/src/components/wikipedia/wikipedia.test.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { render, waitFor, screen, act } from '@testing-library/react';
+import fetchMock from 'jest-fetch-mock';
+
+import { Wikipedia } from './';
+
+describe('<Wikipedia />', () => {
+  beforeAll(() => {
+    fetchMock.enableMocks();
+  });
+
+  test('it renders the component', async () => {
+    fetchMock.mockResponseOnce(async () => {
+      return '<html><body>hello wikipedia !</body></html>';
+    });
+    render(<Wikipedia wikipediaLink={'Teenage_Mutant_Ninja_Turtles'} />);
+
+    act(() => {
+      (window as any).triggerGeneralObserver();
+      return undefined;
+    });
+
+    await waitFor(() => expect(screen.getByTestId('wikipedia')).toBeDefined());
+  });
+});
