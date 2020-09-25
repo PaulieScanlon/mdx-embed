59874871687a0e285c7217d0e755af0eff2e09ae
Rémi Roycourt <remiroycourt@gmail.com>
2020-09-25 02:17:54 +0200

    test(spotify): add unit test

---
+++ packages/mdx-embed/src/components/spotify/spotify.test.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { render, screen, act } from '@testing-library/react';
+
+import { Spotify } from './';
+
+describe('<Spotify />', () => {
+  test('it renders the component', () => {
+    render(<Spotify spotifyLink="album/65KwtzkJXw7oT819NFWmEP" />);
+
+    act(() => {
+      (window as any).triggerGeneralObserver();
+      return undefined;
+    });
+
+    expect(screen.getByTestId('spotify')).toBeDefined();
+  });
+});
