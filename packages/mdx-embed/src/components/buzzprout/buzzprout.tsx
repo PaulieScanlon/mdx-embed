656bb9a91b86296afff3162b8f835018f40b01fe
Matias Hernandez <matiasfh@gmail.com>
2020-09-30 09:40:33 -0300

    95 - Add regex to check buzzsprout id

--- packages/mdx-embed/src/components/buzzprout/buzzprout.tsx
+++ packages/mdx-embed/src/components/buzzprout/buzzprout.tsx
@@ -1,20 +1,32 @@
-import React, { FunctionComponent } from 'react';
-import { GeneralObserver } from '../general-observer';
+import React, { FunctionComponent } from "react";
+import { GeneralObserver } from "../general-observer";
 
 export interface IBuzzproutProps {
-  /* Buzzprout id */
-  buzzproutId: string;
+  /* Buzzprout id: podcastId/episodeId`*/
+  buzzproutId: string,
+  /* width of the iframe: default to 100% */
+  width?: string,
+  /* height of the iframe: default to 200px */
+  height?: string
 }
 
-export const Buzzprout: FunctionComponent<IBuzzproutProps> = ({ buzzproutId }: IBuzzproutProps) => {
+export const Buzzprout: FunctionComponent<IBuzzproutProps> = (
+  { buzzproutId, width = "100%", height = "200" }: IBuzzproutProps
+) => {
+  const regex = /^\d{7}\/\d{7}-[\w|\W]+/;
+  if (!regex.test(buzzproutId)) {
+    return null;
+  }
   return (
     <GeneralObserver>
-      <div style={{ position: 'relative' }}>
+      <div style={{ position: "relative" }}>
         <iframe
           title={`buzzprout-${buzzproutId}`}
-          src={`https://www.buzzsprout.com/${buzzproutId}?client_source=admin&amp;iframe=true`}
-          width="100%"
-          height="200"
+          src={
+            `https://www.buzzsprout.com/${buzzproutId}?client_source=admin&amp;iframe=true`
+          }
+          width={width}
+          height={height}
           frameBorder="0"
           scrolling="no"
         />
