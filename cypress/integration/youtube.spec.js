3a4dad80ec98c58420724aa60ae77e4a9a6936f4
me@jeffersonbledsoe.com <me@jeffersonbledsoe.com>
2020-10-01 16:58:52 +0100

    Remove 'play a video' YouTube test

--- cypress/integration/youtube.spec.js
+++ cypress/integration/youtube.spec.js
@@ -9,13 +9,4 @@ context('<YouTube />', () => {
     getIframeBody().find('.ytp-title-text').should('not.be.undefined');
     getIframeBody().find('.ytp-large-play-button').should('not.be.undefined');
   });
-
-  it('it successfully plays a video', () => {
-    cy.visit('/iframe.html?id=components-youtube--usage&viewMode=story');
-    getIframeBody().find('#player').should('not.be.undefined');
-    // getIframeBody().find('.ytp-large-play-button').should('not.be.undefined');
-    getIframeBody().find('.ytp-large-play-button').click();
-    getIframeBody().find('.ytp-error-icon-container').should('be.undefined');
-    getIframeBody().get('.ytp-chrome-bottom').should('not.be.undefined');
-  });
 });
