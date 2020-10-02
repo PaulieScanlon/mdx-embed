013f1ecbb2f66d0cdaa2632597263777ffdaef60
Alexander Malinov <malinovbox@gmail.com>
2020-10-02 10:22:29 +0300

    add codesandbox unit test (#158)

---
+++ packages/mdx-embed/src/components/codesandbox/codesandbox.test.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { render, screen, act } from '@testing-library/react';
+
+import { CodeSandbox } from './';
+
+describe('<CodeSandbox />', () => {
+  test('it renders the component', () => {
+    const codeSandboxId = 'j0y0vpz59';
+    render(<CodeSandbox codeSandboxId={codeSandboxId} />);
+
+    act(() => {
+      (window as any).triggerGeneralObserver();
+      return undefined;
+    });
+
+    expect(screen.getByTestId('codesandbox')).toBeDefined();
+  });
+});
