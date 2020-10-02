8b27f9e4a5f50d34322ee1f162b338692dd85801
Alexander Malinov <malinovbox@gmail.com>
2020-10-02 09:38:43 +0300

    add flickr unit test

---
+++ packages/mdx-embed/src/components/flickr/flickr.test.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { render, screen, act } from '@testing-library/react';
+
+import { Flickr } from './';
+
+describe('<Flickr />', () => {
+  test('it renders the component', () => {
+    const flickrLink = '65535/50137099548_be9ddb10ce_b.jpg';
+    render(<Flickr flickrLink={flickrLink} />);
+
+    act(() => {
+      (window as any).triggerGeneralObserver();
+      return undefined;
+    });
+
+    expect(screen.getByTestId('flickr')).toBeDefined();
+  });
+});
