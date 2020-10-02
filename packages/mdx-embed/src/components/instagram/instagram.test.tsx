ac952cfb49134027045f10caab515d84b89eef41
Alexander Malinov <malinovbox@gmail.com>
2020-10-02 10:24:33 +0300

    add instagram unit test (#155)

---
+++ packages/mdx-embed/src/components/instagram/instagram.test.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { render, screen, act } from '@testing-library/react';
+
+import { Instagram } from './';
+
+describe('<Instagram />', () => {
+  test('it renders the component', () => {
+    const instagramId = 'CFxh07rFBlQ';
+    render(<Instagram instagramId={instagramId} />);
+
+    act(() => {
+      (window as any).triggerGeneralObserver();
+      return undefined;
+    });
+
+    expect(screen.getByTestId('instagram')).toBeDefined();
+  });
+});
