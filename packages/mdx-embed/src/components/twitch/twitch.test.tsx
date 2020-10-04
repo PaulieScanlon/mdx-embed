92201208b4bb3f8e40225216a27b7d4e7d244eea
Eka <6597211+ekafyi@users.noreply.github.com>
2020-10-04 20:48:19 +0700

    issue 121: make 3 twitch unit tests

--- packages/mdx-embed/src/components/twitch/twitch.test.tsx
+++ packages/mdx-embed/src/components/twitch/twitch.test.tsx
@@ -3,9 +3,42 @@ import { render, screen, act } from '@testing-library/react';
 
 import { Twitch } from './';
 
+const PARENT_PROP = 'http://localhost:6006/';
+
 describe('<Twitch />', () => {
-  test('it renders the component', () => {
-    render(<Twitch twitchId="679474192" />);
+  const intersectionObserverMock = () => ({
+    observe: () => null,
+    unobserve: () => null,
+  });
+
+  beforeEach(() => {
+    window.IntersectionObserver = jest.fn().mockImplementation(intersectionObserverMock);
+  });
+
+  test('it renders the component when provided a twitchId', () => {
+    render(<Twitch parent={PARENT_PROP} twitchId="679474192" />);
+
+    act(() => {
+      (window as any).triggerGeneralObserver();
+      return undefined;
+    });
+
+    expect(screen.getByTestId('twitch')).toBeDefined();
+  });
+
+  test('it renders the component when provided a channel', () => {
+    render(<Twitch parent={PARENT_PROP} channel="jlengstorf" />);
+
+    act(() => {
+      (window as any).triggerGeneralObserver();
+      return undefined;
+    });
+
+    expect(screen.getByTestId('twitch')).toBeDefined();
+  });
+
+  test('it renders the component when provided a collection', () => {
+    render(<Twitch parent={PARENT_PROP} collection="eMybriFryRWpiQ" />);
 
     act(() => {
       (window as any).triggerGeneralObserver();
