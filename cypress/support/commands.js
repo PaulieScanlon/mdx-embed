1347e35f8beea6fbbe271113f48f1882987dce67
Bogdan Lazar <bogdan@bogdanlazar.com>
2020-10-20 19:40:06 +0200

    Add proper Cinnamon test and Cypress command

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,7 +1,18 @@
 const getIframeDocument = () => {
-  return cy.get('iframe').its('0.contentDocument').should('exist');
+  return cy.get('iframe').its('0.contentDocument').should('not.be.empty');
 };
 
-export const getIframeBody = () => {
-  return getIframeDocument().its('body').should('not.be.undefined').then(cy.wrap);
+const getIframeBody = () => {
+  return cy.get('iframe').its('0.contentDocument.body').then(cy.wrap);
 };
+
+Cypress.Commands.add('getIframeBody', () => {
+  cy.log('getIframeBody')
+
+  return cy
+  .get('iframe', { log: false })
+  .its('0.contentDocument.body', { log: false }).should('not.be.empty')
+  .then((body) => cy.wrap(body, { log: false }))
+});
+
+export { getIframeBody };
