dd9419d42830e8b6ca6b70273764873a4f115bb5
Yenly <yencodes@gmail.com>
2020-10-30 17:27:40 -0700

    test: Add twitch integration test and clean up wrong naming in vimeo test (#122)

---
+++ cypress/integration/twitch.spec.js
@@ -0,0 +1,31 @@
+/// <reference types="cypress" />
+
+context('<Twitch />', () => {
+  it('it loads twitch video', () => {
+    cy.visit('/iframe.html?id=components-twitch--usage&viewMode=story');
+
+    cy.get('[data-testid="twitch"]').should('not.be.undefined');
+    cy.getIframeBody().find('.video-player').should('exist');
+    cy.getIframeBody().find('.player-controls').should('exist');
+  })
+
+  it('it loads twitch video skipped to defined time', () => {
+    cy.visit('/iframe.html?id=components-twitch--skip-to&viewMode=story');
+
+    cy.get('[data-testid="twitch"]').should('not.be.undefined');
+    cy.getIframeBody().find('.video-player').should('exist');
+    cy.getIframeBody().find('[aria-label="Play"]').should('exist');
+    cy.getIframeBody().find('.player-controls').should('exist');
+    cy.getIframeBody().find('.tw-absolute').should('not.have.css', 'left', '0%');
+  })
+
+  it('it loads twitch channel', () => {
+    cy.visit('/iframe.html?id=components-twitch--channel&viewMode=story');
+
+    cy.get('[data-testid="twitch"]').should('not.be.undefined');
+    cy.getIframeBody().find('.video-player').should('exist');
+    cy.getIframeBody().find('.player-overlay-background').should('exist');
+    cy.getIframeBody().find('.tw-c-text-overlay').should('exist');
+    cy.getIframeBody().find('.offline-recommendations-video-card').should('exist');
+  })
+})
