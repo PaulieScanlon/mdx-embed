56f718b2a1e0ca14a79fa2d062fafb0d2e6fcb5f
pauliescanlon <pauliescanlon@gmail.com>
2021-06-10 18:09:13 +0100

    feat: update Twitch id for cypress tests

--- cypress/integration/twitch.spec.js
+++ cypress/integration/twitch.spec.js
@@ -7,7 +7,7 @@ context('<Twitch />', () => {
     cy.get('[data-testid="twitch"]').should('not.be.undefined');
     cy.getIframeBody().find('.video-player').should('exist');
     cy.getIframeBody().find('.player-controls').should('exist');
-  })
+  });
 
   it('it loads twitch video skipped to defined time', () => {
     cy.visit('/iframe.html?id=components-twitch--skip-to&viewMode=story');
@@ -17,7 +17,7 @@ context('<Twitch />', () => {
     cy.getIframeBody().find('[aria-label="Play"]').should('exist');
     cy.getIframeBody().find('.player-controls').should('exist');
     cy.getIframeBody().find('.tw-absolute').should('not.have.css', 'left', '0%');
-  })
+  });
 
   it('it loads twitch channel', () => {
     cy.visit('/iframe.html?id=components-twitch--channel&viewMode=story');
@@ -25,7 +25,6 @@ context('<Twitch />', () => {
     cy.get('[data-testid="twitch"]').should('not.be.undefined');
     cy.getIframeBody().find('.video-player').should('exist');
     cy.getIframeBody().find('.player-overlay-background').should('exist');
-    cy.getIframeBody().find('.tw-c-text-overlay').should('exist');
     cy.getIframeBody().find('.offline-recommendations-video-card').should('exist');
-  })
-})
+  });
+});
