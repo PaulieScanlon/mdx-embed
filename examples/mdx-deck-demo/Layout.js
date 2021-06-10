888f3813e87ed731f0738c4db747e12155a70492
Paul Scanlon <pauliescanlon@gmail.com>
2021-06-10 18:42:20 +0100

    feat/gatsby-v3-alpha (#228)
    * feat: upgrade to Gatsby v3.2.0

--- examples/mdx-deck-demo/Layout.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import { MDXEmbedProvider } from 'mdx-embed';
-
-export default ({ children }) => (
-  <div>
-    <MDXEmbedProvider>{children}</MDXEmbedProvider>
-  </div>
-);
