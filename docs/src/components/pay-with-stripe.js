0e01e4b25547c536fb27b5cecdbc66eaca0ffdc5
pauliescanlon <pauliescanlon@gmail.com>
2021-08-29 14:54:20 +0100

    feat: remove origin * from headers

--- docs/src/components/pay-with-stripe.js
+++ docs/src/components/pay-with-stripe.js
@@ -37,7 +37,7 @@ export const PayWithStripe: FunctionComponent = () => {
         },
         {
           headers: {
-            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Origin': 'https://www.mdx-embed.com/',
             'Content-Type': 'application/json',
           },
         },
