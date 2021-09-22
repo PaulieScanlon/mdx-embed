5bd2cc85497752cd96dd8174176c9506e169d61c
Diego Peralta <diego.peralta@amorelie.com>
2021-09-22 15:06:14 +0200

    feat: remove YouTubeNoCookie component and add prop to YouTube one instead

--- packages/mdx-embed/src/components/mdx-embed-provider/components.ts
+++ packages/mdx-embed/src/components/mdx-embed-provider/components.ts
@@ -31,7 +31,6 @@ import { Whimsical } from '../whimsical';
 import { Wikipedia } from '../wikipedia';
 import { Wistia } from '../wistia';
 import { YouTube } from '../youtube';
-import { YouTubeNoCookie } from '../youtube-nocookie';
 
 export const components = {
   AirtableBase,
@@ -68,5 +67,4 @@ export const components = {
   Wikipedia,
   Wistia,
   YouTube,
-  YouTubeNoCookie,
 } as any;
